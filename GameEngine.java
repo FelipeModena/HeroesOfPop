f42ad1886a7e8a7db2944bc3df2bd571a2caa317
Modena <73952306+FelipeModena@users.noreply.github.com>
2021-11-04 23:16:24 -0300

    Create GameEngine.java

---
+++ GameEngine.java
@@ -0,0 +1,30 @@
+
+public class GameEngine {
+    public String userName;
+
+    void setUpGame() {
+        System.out.println("HHH    HH EEEEEE  RRRRRRRRR         OOO         EEEEEEE       SSSSSS    OOOOOOOOO      PPppppppp");
+        System.out.println("HHH    HH EEEEEE  RRRR     RR    OO  (I)  OO    EEEEEEE     SSSSSSS     OO  OO0OO000OO PP    pppp");
+        System.out.println("HHHHHHHHH EE      RRRR     RRR  OOO  (I)   OOO  EEE       SSSSSS        OO  OO OO   OO PP    ppp");
+        System.out.println("HHHHHHHHH EEEEER  RRRRR    RR   OOO  (I)   OOO  EEEEEEE SSSSS           OO  OO OO   OO PP   pp");
+        System.out.println("HHHHHHHHH EEEEER  RRRRR   R     OOO  (I)   OOO  EEEEEEE  SSSSS          OO  OO OO   OO PP pp");
+        System.out.println("HHHHHHHHH EE      RRRRRRR        OO  (I)  OO    EE         SSSSS        OO  OO OO   OO PP");
+        System.out.println("HHH    HH EEEEEEEEEEEE  RRR       OO (I) OO     EEEEEE   SSSSS          OO  OO00OO00OO PP");
+        System.out.println("HHH    HH EEEEEEEEEEEE   RRRR        OOO         EEEEEE SSSS            OOOOOOOOO      PP");
+
+
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        mainMenu();
+
+    }
+
+    private void mainMenu()  {
+
+    }
+}
