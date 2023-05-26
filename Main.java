96cdf77df4f335e5a15f298aedea9bd70e1148a7
Modena <felipe.kroitor@gmail.com>
2023-05-26 18:56:23 -0300

    Menu feature created:

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
 class Main {
     public static void main(String[] args) {
-        GameEngine gameEngine= new GameEngine();
-        gameEngine.startGameLayout();
 
+        gameMenu();
 
+    }
+
+    private static void gameMenu() {
+        int gameSelected = 0;
+        switch (gameSelected) {
+            case 0:
+                GameEngine gameEngine = new GameEngine();
+                gameEngine.startGameLayout();
+                break;
+
+            default:
+            System.out.println("Opção inválida");
+                break;
+        }
     }
 }
